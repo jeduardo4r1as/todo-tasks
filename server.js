556837e863d1014a69b6d2d070fd0e9260c8cc58
Jorge Arias <jorge.arias1996quintero@gmail.com>
2024-01-25 20:51:32 -0500

    Se inicializa el repositorio del servicio web

---
+++ server.js
@@ -0,0 +1,16 @@
+const api = require('./api');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
+app.use('/api', api);
