b9ba6e28f18541a7399a68fb044561644d9f901c
Jorge Arias <jorge.arias1996quintero@gmail.com>
2024-01-25 21:16:16 -0500

    terminar crud y  variables de despliegue

--- api.js
+++ api.js
@@ -2,6 +2,23 @@ var mongoose = require("mongoose");
 var express = require("express");
 var TaskModel = require('./task_schema');
 var router = express.Router();
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
 var query = "mongodb+srv://jorgearias1996quintero:Fundacion2025*@cluster0.wq8xryq.mongodb.net/taskDB?retryWrites=true&w=majority"
 const db = (query);
 
@@ -36,6 +53,40 @@ router.post('/create-task', function (req, res) {
             res.status(500).send("Internal error\n");
         }
         else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.get('/all-tasks', function (req, res) {
+    TaskModel.find(function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
             res.status(200).send("OK\n");
         }
     });
